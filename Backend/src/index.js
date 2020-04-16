b1e046e8a9694ca3a4197ba582a3b8b1615c4757
decipher07 <social.deej@gmail.com>
2020-04-16 14:01:20 +0530

    Setting Separate Route Files

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser') 
-require('./DataBase/mongoose')
-const User = require('./models/UserLoginAndRegister')
-const hash = require('./middleware/hash')
-const checkAuth = require('./middleware/checkAuth')
+const loginAndRegisterRoutes = require('./routers/LoginAndRegisterRoutes')
+const fetchRoutesModels = require('./routers/FetchRoutesModels')
 
 const app = express();
 const port = process.env.PORT || 3000 
@@ -11,13 +9,8 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.post('/register',hash, (req, res) => {
-    res.status(201).send(req.body)
-})
-
-app.post('/login', checkAuth, (req, res) => {
-    res.status(401).send()
-})
+app.use(fetchRoutesModels)
+//app.use(loginAndRegisterRoutes)
 
 app.listen(port , (req, res) => {
     console.log(`Server Running at Port ${port}`)
